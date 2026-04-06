11eb55dbafe4a0090675f57b975f39f17becc1d2
Ace5tar <tace233@gmail.com>
2026-04-05 23:44:57 -0700

    added commands to interface with tree

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,18 @@ int initializeRandom(BinarySearchTree*& bst);
 // Set setInt from char*, checks if input is valid
 int intFromCstring(int& setInt, char* cstring);
 
+// Insert an element into the binary search tree
+int insertCommand(BinarySearchTree*& bst);
+
+// Remove an element from the binary search tree
+int removeCommand(BinarySearchTree*& bst);
+
+// Check if an element exists in the binary search tree
+int searchCommand(BinarySearchTree*& bst);
+
+// Print the help text
+int printHelp();
+
 int main() {
 
   char in[64];
@@ -28,6 +40,7 @@ int main() {
   std::cout << "[s] From string" << std::endl;
   std::cout << "[r] Randomly"<< std::endl;
   std::cout << "[e] Empty" << std::endl;
+  std::cout << "[q] Quit program" << std::endl;
 
   std::cout << " > ";
   std::cin.getline(in, 64);
@@ -80,13 +93,76 @@ int main() {
 
   } else if (strcmp(in, "e") == 0) {
     bst = new BinarySearchTree(nullptr, 0);
+
+  } else if (strcmp(in, "q") == 0) {
+    return 0;
   } else {
     std::cout << "ERROR: Unrecognized initializer." << std::endl;
     return 1;
   }
 
+  std::cout << "Initialization complete" << std::endl;
 
-  std::cout << *bst << std::endl;
+  std::cout << "Use \"help\" to see a list of commands" << std::endl;
+
+  bool running = true;
+
+  while (running) {
+
+    std::cout << " > ";
+    std::cin.getline(in, 64);
+
+    if (strcmp(in, "p") == 0 || strcmp(in, "print") == 0) {
+      std::cout << *bst << std::endl;
+    }
+
+    if (strcmp(in, "q") == 0 || strcmp(in, "quit") == 0) {
+      running = false;
+    }
+
+    if (strcmp(in, "h") == 0 || strcmp(in, "help") == 0) {
+      printHelp();
+    }
+
+    if (strcmp(in, "i") == 0 || strcmp(in, "insert") == 0) {
+
+      errorCode = insertCommand(bst);
+
+      if (errorCode == 1) {
+        std::cout << "Error while inserting element to tree." << std::endl;
+      }
+      
+      if (errorCode == 2) {
+        std::cout << "Error while converting input to int." << std::endl;
+      }
+
+    }
+
+    if (strcmp(in, "r") == 0 || strcmp(in, "remove") == 0) {
+
+      errorCode = removeCommand(bst);
+
+      if (errorCode == 1) {
+        std::cout << "Error while removing element from tree." << std::endl;
+      }
+      
+      if (errorCode == 2) {
+        std::cout << "Error while converting input to int." << std::endl;
+      }
+
+    }
+
+    if (strcmp(in, "s") == 0 || strcmp(in, "search") == 0) {
+
+      errorCode = searchCommand(bst);
+
+      if (errorCode == 1) {
+        std::cout << "Error while converting input to int." << std::endl;
+      }
+    }
+  }
+
+
 
   delete bst;
 }
@@ -111,7 +187,7 @@ int intFromCstring(int& setInt, char* cstring) {
 
 // Error code 1: input was empty
 // Error code 2: error converting input to int 
-// Error code 3: failed opening file
+// Erro code 3: failed opening file
 int initializeFromFile(BinarySearchTree*& bst) {
 
   char in[64];
@@ -206,4 +282,82 @@ int initializeRandom(BinarySearchTree*& bst) {
   bst = new BinarySearchTree(dataArray, count);
 
   return 0;
+}
+
+// Error code 1: error inserting data
+// Error code 2: error converting to int
+int insertCommand(BinarySearchTree*& bst) {
+
+  char in[64];
+
+  int errorCode;
+  int data;
+
+  std::cout << "Element to insert:" << std::endl;
+  std::cout << " > ";
+  std::cin.getline(in, 64);
+
+  errorCode = intFromCstring(data, in);
+  if (errorCode) { return 2; }
+
+  return bst->insert(data);
+
+}
+
+// Error code 1: error removing data
+// Error code 2: error converting to int
+int removeCommand(BinarySearchTree*& bst) {
+
+  char in[64];
+
+  int errorCode;
+  int data;
+
+  std::cout << "Element to remove:" << std::endl;
+  std::cout << " > ";
+  std::cin.getline(in, 64);
+
+  errorCode = intFromCstring(data, in);
+  if (errorCode) { return 2; }
+
+  return bst->remove(data);
+}
+
+// Error code 1: error converting to int
+int searchCommand(BinarySearchTree*& bst) {
+
+  char in[64];
+
+  int errorCode;
+  int data;
+
+  std::cout << "Element to search for:" << std::endl;
+  std::cout << " > ";
+  std::cin.getline(in, 64);
+
+  errorCode = intFromCstring(data, in);
+  if (errorCode) { return 1; }
+
+  if (bst->search(data)) {
+    std::cout << "Element \"" << in << "\" exists in tree." << std::endl;
+  } else {
+    std::cout << "Element \"" << in << "\" does not exist in tree." << std::endl;
+  }
+
+  return 0;
+}
+
+// Print the help text
+int printHelp() {
+
+  std::cout << "Commands:" << std::endl;
+  std::cout << "PRINT [\"p\", \"print\"] - Prints the binary search tree." << std::endl;
+  std::cout << "INSERT [\"i\", \"insert\"] - Insert an element into the binary search tree." << std::endl;
+  std::cout << "REMOVE [\"r\", \"remove\"] - Remove an element from the binary search tree." << std::endl;
+  std::cout << "SEARCH [\"s\", \"search\"] - See if an element exists in the binary search tree." << std::endl;
+  std::cout << "HELP [\"h\", \"help\"] - Show this help text." << std::endl;
+  std::cout << "QUIT [\"q\", \"quit\"] - Quit the program." << std::endl;
+
+  return 0;
+
 }
